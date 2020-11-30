4a98c190ab7bb25bce3c15ec5f51c577d9821795
Abdul Khan <akhan@lcs.on.ca>
2020-11-30 15:03:58 -0500

    Initial Commit

---
+++ Ww2Timeline/Ww2TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ww2TimelineApp.swift
+//  Ww2Timeline
+//
+//  Created by Abdul Ahad Khan on 2020-11-30.
+//
+
+import SwiftUI
+
+@main
+struct Ww2TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
